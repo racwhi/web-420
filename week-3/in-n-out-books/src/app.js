345858d5767fdcb171fa6d8b7d5bd38430025c97
racwhi <128566218+racwhi@users.noreply.github.com>
2025-01-26 22:05:29 -0600

    week 3 assignment

--- week-3/in-n-out-books/src/app.js
+++ week-3/in-n-out-books/src/app.js
@@ -24,18 +24,88 @@ app.get("/", (req, res) => {
       <head>
         <title>In-N-Out-Books</title>
         <style>
-          body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-          }
+         body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+    color: #333;
+}
+
+header {
+    background: gray;
+    color: #ffffff;
+    padding: 20px 0;
+    text-align: center;
+}
+
+main {
+    padding: 20px;
+}
+
+div {
+    margin: 20px 0;
+    padding: 15px;
+    background: #ffffff;
+    border-radius: 5px;
+
+}
+
+
+
+footer {
+    text-align: center;
+    padding: 10px 0;
+    background: gray;
+    color: black;
+    position: relative;
+    bottom: 0;
+    width: 100%;
+}
+
         </style>
       </head>
       <body>
-        <h1>Welcome to In-N-Out-Books</h1>
-        <p>A platform for managing your book collections.</p>
-      </body>
-    </html>
-  `;
+
+<header>
+    <h1>Welcome to In-N-Out Books</h1>
+</header>
+
+<main>
+    <div id="introduction">
+        <h2>About Us</h2>
+        <p>At In-N-Out Books, books and people are our passion. If you're looking, more than likely we have it and if we don't, we can get it. Everyone's welcome , from one book lover to another!</p>
+    </div>
+
+    <div id="top-selling-books">
+        <h2>Top Selling Book(s)</h2>
+        <ul>
+            <li>The Bible</li>
+        </ul>
+    </div>
+
+    <div id="hours-of-operation">
+        <h2>Hours of Operation</h2>
+        <p>Monday - Friday: 10 AM - 8PM CT</p>
+        <p>Saturday: 10 AM - 6 PM CT</p>
+        <p>Sunday: Closed</p>
+    </div>
+
+    <div id="contact-information">
+        <h2>Contact Us</h2>
+        <p>Email: contactus@innoutbooks.com</p>
+        <p>Phone: (555) 555-1234</p>
+        <p>Address: 5557 N Book St., Brooklyn, NY 12345</p>
+    </div>
+</main>
+
+<footer>
+    <p>&copy; 2025 In-N-Out Books. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
+ `;
   res.send(html);
 });
 
